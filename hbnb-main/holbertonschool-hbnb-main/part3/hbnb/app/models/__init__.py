0aa3d874596abf4eac90b4069bc4dc172b0a4bcc
loties <alexislaubert@yahoo.fr>
2026-04-23 22:07:23 +0200

    chore: nettoyage du dépôt (suppression hbnb et cache)

--- hbnb-main/holbertonschool-hbnb-main/part3/hbnb/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .basemodel import BaseModel
-from .user import User
-from .place import Place
-from .review import Review
-from .amenity import Amenity
-
-__all__ = ['BaseModel', 'User', 'Place', 'Review', 'Amenity']
